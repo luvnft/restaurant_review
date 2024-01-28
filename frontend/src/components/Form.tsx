c488523902b1f1110a5889fb146d01e3dbfec4a6
The Wizard of Hahz <info@blkluv.org>
2024-01-28 03:55:51 -0500

    location

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -86,7 +86,7 @@ const ReviewForm: FC<FormProps> = ({
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="location"
               type="text"
-              placeholder="///what.3.bars"
+              placeholder="location"
               value={location}
               onChange={(e) => setLocation(e.target.value)}
             />
