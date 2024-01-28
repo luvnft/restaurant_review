aafdbe3d3b149d1269208c001f9dfdedb0ac8f0d
The Wizard of Hahz <info@blkluv.org>
2024-01-28 03:34:17 -0500

    updated

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -34,77 +34,88 @@ const ReviewForm: FC<FormProps> = ({
     handleSubmit();
   };
   return (
-    <form
-      className="shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      onSubmit={(e) => formSubmit(e)}
-    >
-      <div className="w-full max-w-xs">
-        <div className="mb-4">
-          <label className="block text-gray-400 text-sm font-bold mb-2">
-            Title
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="title"
-            type="text"
-            placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-400 text-sm font-bold mb-2">
-            Description
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="description"
-            type="text"
-            placeholder="Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-400 text-sm font-bold mb-2">
-            Location
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="location"
-            type="text"
-            placeholder="Location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-400 text-sm font-bold mb-2">
-            Rating
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="rating"
-            type="number"
-            placeholder="Description"
-            value={rating}
-            onChange={(e) => setRating(Number(e.target.value))}
-            max={10}
-            min={0}
-          />
-        </div>
-
-        <div className="flex items-center justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            {variant === ReviewVariant.add ? "Add Review" : "Edit Review"}
-          </button>
-        </div>
+    <div>
+      <div className="flex justify-center items-center mb-4">
+        {/* Logo with text */}
+        <img
+          src="/images/solview.webp"
+          alt="SOLVIEW Logo"
+          className="h-10 mr-2"
+        />
+        <h1 className="text-2xl font-bold">SOLVIEW</h1>
       </div>
-    </form>
+      <form
+        className="shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={(e) => formSubmit(e)}
+      >
+        <div className="w-full max-w-xs">
+          <div className="mb-4">
+            <label className="block text-gray-400 text-sm font-bold mb-2">
+              Title
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="title"
+              type="text"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+  
+          <div className="mb-4">
+            <label className="block text-gray-400 text-sm font-bold mb-2">
+              Description
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="description"
+              type="text"
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+  
+          <div className="mb-4">
+            <label className="block text-gray-400 text-sm font-bold mb-2">
+              Location
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="location"
+              type="text"
+              placeholder="Location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+  
+          <div className="mb-4">
+            <label className="block text-gray-400 text-sm font-bold mb-2">
+              Rating
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="rating"
+              type="number"
+              placeholder="Description"
+              value={rating}
+              onChange={(e) => setRating(Number(e.target.value))}
+              max={10}
+              min={0}
+            />
+          </div>
+  
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              {variant === ReviewVariant.add ? "Add Review" : "Edit Review"}
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
-export default ReviewForm;
+  export default ReviewForm;
